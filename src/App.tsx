1a67c7966ccda6ef2a1194bf3fb6fc9d51520401
Đặng Hoàng Nguyên <yorichi@MacBookPro.bbrouter>
2024-10-08 18:04:31 +0700

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
-import { useEffect, useRef } from "react";
-import { dia, shapes, ui } from "joinjs-plus";
-import "./App.css";
-
-function App() {
-  const canvas = useRef<Element | null>(null);
-
-  useEffect(() => {
-    const graph = new dia.Graph({}, { cellNamespace: shapes });
-
-    const paper = new dia.Paper({
-      model: graph,
-      background: {
-        color: "#F8F9FA",
-      },
-      frozen: true,
-      async: true,
-      cellViewNamespace: shapes,
-    });
-
-    const scroller = new ui.PaperScroller({
-      paper,
-      autoResizePaper: true,
-      cursor: "grab",
-    });
-    canvas.current?.appendChild(scroller.el);
-    scroller.render().center();
-    const rect = new shapes.standard.Rectangle({
-      position: { x: 100, y: 30 },
-      size: { width: 100, height: 40 },
-      attrs: {
-        label: {
-          text: "Hello Nguyen",
-        },
-      },
-    });
-    graph.addCell(rect);
-    paper.unfreeze();
-    return () => {
-      scroller.remove();
-      paper.remove();
-    };
-  }, []);
-
-  return <div className="canvas" ref={canvas} />;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
